743c0a89814fca02ef61c7ef995925b6d8a416ec
Claudio Carnino <claudio@tugulab.org>
2022-12-08 18:16:52 +0000

    Complete example of the video player

--- App.js
+++ App.js
@@ -1,6 +1,18 @@
+import {
+  LivepeerConfig,
+  createReactClient,
+  studioProvider,
+} from '@livepeer/react-native';
 import HomeScreen from './HomeScreen';
 
-const App = () => (<HomeScreen />)
+const client = createReactClient({
+ provider: studioProvider({ apiKey: 'c7d0ba9f-3666-4b37-bb71-47473b3290ca' }),
+});
+
+const App = () => (
+  <LivepeerConfig client={client}>
+    <HomeScreen />
+  </LivepeerConfig>
+)
 
 export default App;
-
--- HomeScreen.js
+++ HomeScreen.js
@@ -1,3 +1,4 @@
+import { Player, useAsset } from "@livepeer/react-native";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
@@ -10,11 +11,17 @@ const styles = StyleSheet.create({
   },
 });
 
-const HomeScreen = () => (
-  <View style={styles.container}>
-    <Text>Home screen</Text>
-    <StatusBar style="auto" />
-  </View>
-);
+const assetId = 'd29f9566-6630-471f-9b71-bc7824039e72'
+
+const HomeScreen = () => {
+  const { data: asset, isLoading } = useAsset({ assetId });
+  if (isLoading) return <Text>Loading...</Text>;
+  return (
+    <View style={styles.container}>
+      <Text>Asset: {asset.name}</Text>
+      <Player playbackId={asset.playbackId} />
+    </View>
+  );
+};
 
 export default HomeScreen;
